634d3e59766aa8fba2bc1c2478ed3df6070c7710
praabindhp <praabindhp@gmail.com>
2021-03-17 13:00:34 +0530

    Updated

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from typing import Counter
 import csv
 import pandas as pd
+import names
 
 # Inputs :: Source & Destination
 inputFileName = "Titanic.csv"
@@ -12,22 +13,26 @@ outputFileName = os.path.splitext(inputFileName)[0] + "_modified.csv"
 
 # Reading CSV File
 df = pd.read_csv(inputFileName)
-  
+
 # Creating List of Column Names
 list_of_column_names = list(df.columns)
   
 # Displaying List of Column Names 
-print('List of column names : ',  
-      list_of_column_names)
+# print('List of Column Names : ', list_of_column_names)
+
+random_names = []
+for header in list_of_column_names: # Encopted List With Random Names
+    random_names.append(names.get_first_name())
+print(random_names)
 
 # Converting List To String
 changed_header_initial = ''
-for header in list_of_column_names:
-    changed_header_initial = str(changed_header_initial+','+header)
+for name in random_names:
+    changed_header_initial = str(changed_header_initial+','+name)
 
 # String - Changed Headers
 changed_header_initial = changed_header_initial[1:]
-print(changed_header_initial)
+# print(changed_header_initial)
 
 # Appending The Headers To The Modified CSV File
 with open(outputFileName, "w") as outfile:
